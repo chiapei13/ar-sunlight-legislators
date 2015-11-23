b6d4bc69af93e75596b25cfd7c6544fd003f14d1
Chia Pei Cheok <chiapei13@gmail.com>
2015-11-23 17:09:24 +0800

    Update import

---
+++ app/models/legislator.rb
@@ -0,0 +1,17 @@
+require_relative '../../db/config'
+
+class Legislator < ActiveRecord::Base
+
+	# validation => validates	:first_name, :last_name, :email, :phone, presence: true
+	# validation => validates	:email, uniqueness: true	# add :name join?
+	# ARA => has_one	:XXX
+	
+	
+end
+
+#CRUD
+
+
+
+
+
